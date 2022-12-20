08251fa285703ba2f89c130b99962bcfef3fa16e
zhuowei <cheukwaylee@gmail.com>
2022-12-20 20:09:00 +0100

    TODO merge GIN into GNN-HLS

---
+++ models/GIN_usingPYG.py
@@ -0,0 +1,73 @@
+# https://mlabonne.github.io/blog/gin/
+
+import torch
+import torch.nn.functional as F
+from torch.nn import Linear, Sequential, BatchNorm1d, ReLU, Dropout
+from torch_geometric.nn import GCNConv, GINConv
+from torch_geometric.nn import global_mean_pool, global_add_pool
+
+
+class GCN(torch.nn.Module):
+    """GCN"""
+    def __init__(self, dim_h):
+        super(GCN, self).__init__()
+        self.conv1 = GCNConv(dataset.num_node_features, dim_h)
+        self.conv2 = GCNConv(dim_h, dim_h)
+        self.conv3 = GCNConv(dim_h, dim_h)
+        self.lin = Linear(dim_h, dataset.num_classes)
+
+    def forward(self, x, edge_index, batch):
+        # Node embeddings 
+        h = self.conv1(x, edge_index)
+        h = h.relu()
+        h = self.conv2(h, edge_index)
+        h = h.relu()
+        h = self.conv3(h, edge_index)
+
+        # Graph-level readout
+        hG = global_mean_pool(h, batch)
+
+        # Classifier
+        h = F.dropout(hG, p=0.5, training=self.training)
+        h = self.lin(h)
+        
+        return hG, F.log_softmax(h, dim=1)
+
+class GIN(torch.nn.Module):
+    """GIN"""
+    def __init__(self, dim_h):
+        super(GIN, self).__init__()
+        self.conv1 = GINConv(
+            Sequential(Linear(dataset.num_node_features, dim_h),
+                       BatchNorm1d(dim_h), ReLU(),
+                       Linear(dim_h, dim_h), ReLU()))
+        self.conv2 = GINConv(
+            Sequential(Linear(dim_h, dim_h), BatchNorm1d(dim_h), ReLU(),
+                       Linear(dim_h, dim_h), ReLU()))
+        self.conv3 = GINConv(
+            Sequential(Linear(dim_h, dim_h), BatchNorm1d(dim_h), ReLU(),
+                       Linear(dim_h, dim_h), ReLU()))
+        self.lin1 = Linear(dim_h*3, dim_h*3)
+        self.lin2 = Linear(dim_h*3, dataset.num_classes)
+
+    def forward(self, x, edge_index, batch):
+        # Node embeddings 
+        h1 = self.conv1(x, edge_index)  # res dim = D
+        h2 = self.conv2(h1, edge_index) # res dim = D
+        h3 = self.conv3(h2, edge_index) # res dim = D
+
+        # Graph-level readout
+        h1 = global_add_pool(h1, batch)  # res dim = D
+        h2 = global_add_pool(h2, batch)  # res dim = D
+        h3 = global_add_pool(h3, batch)  # res dim = D
+
+        # Concatenate graph embeddings
+        h = torch.cat((h1, h2, h3), dim=1)  # graph-level embeddings, res dim = 3*D
+
+        # Classifier
+        h = self.lin1(h)
+        h = h.relu()
+        h = F.dropout(h, p=0.5, training=self.training)
+        h = self.lin2(h)
+        
+        return h, F.log_softmax(h, dim=1)
--- models/mlp.py
+++ models/mlp.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-###MLP with lienar output
+### MLP with lienar output: (FC+BN+relu) x N + FC, N = 0, 1, 2...
 class MLP(nn.Module):
     def __init__(self, num_layers, input_dim, hidden_dim, output_dim):
         '''
@@ -10,12 +10,10 @@ class MLP(nn.Module):
             input_dim: dimensionality of input features
             hidden_dim: dimensionality of hidden units at ALL layers
             output_dim: number of classes for prediction
-            device: which device to use
         '''
     
         super(MLP, self).__init__()
-
-        self.linear_or_not = True #default is linear model
+        self.linear_or_not = True  #default is linear model
         self.num_layers = num_layers
 
         if num_layers < 1:
@@ -29,21 +27,21 @@ class MLP(nn.Module):
             self.linears = torch.nn.ModuleList()
             self.batch_norms = torch.nn.ModuleList()
         
-            self.linears.append(nn.Linear(input_dim, hidden_dim))
+            self.linears.append(nn.Linear(input_dim, hidden_dim))  # first layer
             for layer in range(num_layers - 2):
-                self.linears.append(nn.Linear(hidden_dim, hidden_dim))
-            self.linears.append(nn.Linear(hidden_dim, output_dim))
+                self.linears.append(nn.Linear(hidden_dim, hidden_dim))  # intermediate layers
+            self.linears.append(nn.Linear(hidden_dim, output_dim))  # last layer
 
             for layer in range(num_layers - 1):
-                self.batch_norms.append(nn.BatchNorm1d((hidden_dim)))
+                self.batch_norms.append(nn.BatchNorm1d((hidden_dim)))  # the last layer do not need BN
 
     def forward(self, x):
         if self.linear_or_not:
             #If linear model
-            return self.linear(x)
+            return self.linear(x)  # only one layer: only FC
         else:
             #If MLP
             h = x
             for layer in range(self.num_layers - 1):
-                h = F.relu(self.batch_norms[layer](self.linears[layer](h)))
-            return self.linears[self.num_layers - 1](h)
+                h = F.relu(self.batch_norms[layer](self.linears[layer](h)))  # FC+BN+relu
+            return self.linears[self.num_layers - 1](h)  # last layer: only FC
